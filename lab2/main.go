855ee3df4111c482229949508951790d9367fbed
Sanyoq <60144890+Ssanyoq@users.noreply.github.com>
2025-05-11 12:47:48 +0300

    feat: partially implement user interface

--- lab2/main.go
+++ lab2/main.go
@@ -1,17 +1,8 @@
 package main
 
+import "fmt"
+
 func main() {
-	// p := NewParser(NewLexer("a(a|b)*"))
-	// nfa, err := p.BuildNFA()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	nfa := ConcatAutomata(CharAutomata('a'), KleeneeAutomata(OrAutomata(CharAutomata('a'), CharAutomata('b'))))
-	nfa.PrintAutomata()
-	println("DFA:")
-	dfa := GenerateDFA(nfa)
-	dfa.PrintDFA()
-	println("Minimized:")
-	dfa.Minimize()
-	dfa.PrintDFA()
+	re := NewRegexMachine("aboba%.")
+	fmt.Println(re.Match("aboba."))
 }
--- lab2/regex_machine.go
+++ lab2/regex_machine.go
@@ -2,24 +2,40 @@ package main
 
 import "errors"
 
+// User interface for regex library
 type RegexMachine struct {
 	pattern  string
-	automata *Automata
+	automata *DFAResult
 }
 
+// Creates new instance of RegexMachine
+func NewRegexMachine(pattern string) *RegexMachine {
+	return &RegexMachine{
+		pattern: pattern,
+	}
+}
+
+// Compiles pattern into minimized DFA
 func (r *RegexMachine) Compile() error {
-	r.pattern = "not implemented"
-	return errors.New("not implemented")
+	p := NewParser(NewLexer(r.pattern))
+	nfa, err := p.BuildNFA()
+	if err != nil {
+		return err
+	}
+	r.automata = GenerateDFA(nfa)
+	return r.automata.Minimize()
 }
 
-func (r *RegexMachine) Match(matchee string) (bool, error) {
+// Returns longest match of matchee.
+// Also compiles automata by pattern if not compiled yet
+func (r *RegexMachine) Match(matchee string) (string, error) {
 	if r.automata == nil {
 		err := r.Compile()
 		if err != nil {
-			return false, err
+			return "", err
 		}
 	}
-	return false, errors.New("not implemented")
+	return r.automata.Match(matchee), nil
 }
 
 // Recovers pattern from
